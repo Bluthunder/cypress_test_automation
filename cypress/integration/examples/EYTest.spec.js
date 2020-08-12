6a0754aa15e886b9a6e802b4abdea7e3dfbe189a
Nemo <Bluthunder@users.noreply.github.com>
2020-08-12 23:49:40 +0400

    get added

--- cypress/integration/examples/EYTest.spec.js
+++ cypress/integration/examples/EYTest.spec.js
@@ -5,7 +5,7 @@ describe('Execute automation homepage',()=>{
     it('It should launch execute automation homepage',()=>{
 
         cy.visit("http://www.executeautomation.com/site/")
-        cy.get("")
+        cy.get("input[type='text'][placeholder*='learning']").type("Cypress")
         
     })
 })
